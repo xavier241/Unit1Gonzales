d1294bf9206b579fc718f1cbf5c0c0bc2d8033bc
xavier241 <Xaviergonzales009@gmail.com>
2025-03-14 09:58:49 -0700

    Done
    Done

--- U1PlayerControlGonzales/Assets/Scripts/PlayerController.cs
+++ U1PlayerControlGonzales/Assets/Scripts/PlayerController.cs
@@ -1,9 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class PlayerController : MonoBehaviour
 {
+    public Camera mainCamera; 
+    public Camera hoodCamera;
+    public KeyCode switchKey;
     private float speed = 20.0f;
     private float turnspeed = 45.0f;
     private float horizontalInput;
@@ -17,10 +21,19 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+       
         forwardInput = Input.GetAxis("Vertical");
         horizontalInput = Input.GetAxis("Horizontal");
         // Move the vehicle forward
         transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
-        transform.Translate(Vector3.right * Time.deltaTime * turnspeed * horizontalInput);
+        transform.Rotate(Vector3.up,turnspeed * horizontalInput * Time.deltaTime);
+        if (Input.GetKey(switchKey)) 
+        {
+            mainCamera.enabled = !mainCamera.enabled;
+            hoodCamera.enabled = !hoodCamera.enabled;
+
+        }
+
+       
     }
 }
